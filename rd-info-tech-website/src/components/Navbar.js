967e4ac5989d328a12f1b7ad62015613e8d22008
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-05-06 15:51:42 -0700

    Navbar component - initial setup

---
+++ rd-info-tech-website/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+      <div className='left-navbar'>
+        <img src={require('../images/sample-logo.png')} />
+        <div>RD Info Tech</div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Navbar
