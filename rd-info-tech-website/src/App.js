c629f9ab3f6da8036a9d0684d9a558f2e5fcf95c
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-05-10 11:02:39 -0700

    Create Footer component

--- rd-info-tech-website/src/App.js
+++ rd-info-tech-website/src/App.js
@@ -1,11 +1,15 @@
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 import './App.css';
 
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
   );
 }
 
