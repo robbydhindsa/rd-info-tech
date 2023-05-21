8673f1e4482f2befb565eac51fc2ed04e6742f29
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-05-21 09:53:19 -0700

    Configure and style Navbar

--- rd-info-tech-website/src/App.js
+++ rd-info-tech-website/src/App.js
@@ -1,13 +1,33 @@
+import React from 'react'
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 import './App.scss';
 
 
 function App() {
+  console.log(window.location);
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home/>
+      break
+    case "/about":
+      component = <About/>
+      break
+    case "/contact":
+      component = <Contact/>
+      break
+  }
+
   return (
     <div className='page-container'>
       <Navbar/>
+      {component}
       <Footer/>
     </div>
   );
--- rd-info-tech-website/src/components/Navbar.js
+++ rd-info-tech-website/src/components/Navbar.js
@@ -1,22 +1,32 @@
 import React from 'react'
+
 import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='container'>
+    <div className='navbar-container'>
       <div className='left-navbar'>
-        <img
-          className='logo'
-          src={require('../images/sample-logo.png')}
-          alt=''
-        />
-        <div>RD Info Tech</div>
+        <a href='/'>
+          <img
+            className='logo'
+            src={require('../images/sample-logo.png')}
+            alt=''
+          />
+        </a>
+        <div className='logo-text'>RD Info Tech</div>
       </div>
 
       <div className='right-navbar'>
-        <a href=''>Home</a>
+        <ul>
+          {/* <li><a href='/home'>Home</a></li> */}
+          <li><a href='/about'>About</a></li>
+          <li><a href='/contact'>Contact</a></li>
+        </ul>
+
+
+        {/* <a href=''>Home</a>
         <a href=''>About</a>
-        <a href=''>Contact</a>
+        <a href=''>Contact</a> */}
       </div>
     </div>
 
