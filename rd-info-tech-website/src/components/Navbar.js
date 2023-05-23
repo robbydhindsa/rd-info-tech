69a2b602e09ef08453bc604a9f21c1cc065066c0
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-05-23 14:27:26 -0700

    Use react-router-dom for routing instead of switch statements

--- rd-info-tech-website/src/components/Navbar.js
+++ rd-info-tech-website/src/components/Navbar.js
@@ -1,26 +1,29 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom';
+
+
 import "./navbar.scss";
 
 const Navbar = () => {
   return (
     <div className='navbar-container'>
       <div className='left-navbar'>
-        <a href='/'>
+        <Link to='/'>
           <img
             className='logo'
             src={require('../images/sample-logo.png')}
             alt=''
           />
-        </a>
+        </Link>
         <div className='logo-text'>RD Info Tech</div>
       </div>
 
       <div className='right-navbar'>
         <ul>
           {/* <li><a href='/home'>Home</a></li> */}
-          <li><a href='/about'>About</a></li>
-          <li><a href='/contact'>Contact</a></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
         </ul>
 
 
