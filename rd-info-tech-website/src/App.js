19144375c4150954358ecef8199a0859f65720f4
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-05-23 14:19:50 -0700

    Uninstall/reinstall react-router-dom in correct directory

--- rd-info-tech-website/src/App.js
+++ rd-info-tech-website/src/App.js
@@ -6,28 +6,37 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 
+import { Route, Routes } from 'react-router-dom';
+
 import './App.scss';
 
 
 function App() {
-  console.log(window.location);
-  let component;
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home/>
-      break
-    case "/about":
-      component = <About/>
-      break
-    case "/contact":
-      component = <Contact/>
-      break
-  }
+  // console.log(window.location);
+  // let component;
+  // switch (window.location.pathname) {
+  //   case "/":
+  //     component = <Home/>
+  //     break
+  //   case "/about":
+  //     component = <About/>
+  //     break
+  //   case "/contact":
+  //     component = <Contact/>
+  //     break
+  // }
 
   return (
     <div className='page-container'>
       <Navbar/>
-      {component}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+      {/* {component} */}
+      
       <Footer/>
     </div>
   );
