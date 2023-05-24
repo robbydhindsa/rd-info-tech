3999a52f8acb723abb47200191e533c5f2d5ad32
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-05-23 18:15:09 -0700

    Add content and styling to Home component

--- rd-info-tech-website/src/components/Home.js
+++ rd-info-tech-website/src/components/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import "./home.scss";
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homepage-container'>
+      <img
+        className='homepage-logo'
+        src={require('../images/sample-logo.png')}
+        alt=''
+      />
+      <h1>RD Info Tech Consulting</h1>
+    </div>
   )
 }
 
