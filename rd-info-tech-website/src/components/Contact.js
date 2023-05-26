299f366050e9e0799bb1f967c3f6b012900af6bd
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-05-25 21:45:50 -0700

    Configure contact form in Contact component

--- rd-info-tech-website/src/components/Contact.js
+++ rd-info-tech-website/src/components/Contact.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 import './contact.scss';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      process.env.REACT_APP_SERVICE_ID,
+      process.env.REACT_APP_TEMPLATE_ID,
+      form.current,
+      process.env.REACT_APP_PUBLIC_KEY
+    )
+      .then((result) => {
+          console.log(result.text);
+          console.log("Message sent!");
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div className='contact-container'>
       <div className='contact-logo'>
@@ -11,9 +33,17 @@ const Contact = () => {
           alt=''
         />
       </div>
-      <div className='contact-description'>
-        <h1>Contact</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id sagittis ipsum. Cras tempus malesuada turpis a accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur dui in nunc bibendum dignissim. Mauris malesuada nisi nisi, nec imperdiet mauris pretium ac. Nunc commodo, magna nec rhoncus auctor, turpis dui congue dui, at maximus ligula quam at enim. Proin neque felis, tempor a hendrerit vitae, molestie sed velit. Nunc ut risus magna. In tincidunt nibh sapien, eu mollis nibh volutpat at. In placerat eu nunc in gravida. Phasellus sed interdum neque, quis viverra orci. Maecenas sollicitudin sollicitudin tempus. Fusce neque risus, cursus congue iaculis quis, elementum nec nulla. Pellentesque dapibus pellentesque justo, vitae scelerisque est tristique sit amet. Nullam ut porttitor neque, et rutrum odio.</p>
+      <div className='contact-form-container'>
+        <h2>Contact</h2>
+        <form ref={form} onSubmit={sendEmail} className='contact-form'>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
       </div>
     </div>
   )
